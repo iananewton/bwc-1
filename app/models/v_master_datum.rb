6fac8743b634f4fc760ba847f2887dbcf3b5bacc
David J Watts <davidjeremywatts@gmail.com>
2016-12-05 17:03:50 -0700

    fine tune views

--- app/models/v_master_datum.rb
+++ app/models/v_master_datum.rb
@@ -1,3 +1,7 @@
 class VMasterDatum < ActiveRecord::Base
   self.primary_key = "id"
+
+  belongs_to :MDSubtype, foreign_key: "MDSubtypeId", class_name: "MasterDataSubtype"
+  belongs_to :Vessel, foreign_key: "VesselId", class_name: "Vessel"
+  has_one :MDType, :through => :MDSubtype
 end
--- app/models/v_vessel.rb
+++ app/models/v_vessel.rb
@@ -1,3 +1,6 @@
 class VVessel < ActiveRecord::Base
   self.primary_key = "id"
+
+  belongs_to :VesselType, foreign_key: "VesselTypeId", class_name: "VesselType"
+
 end
